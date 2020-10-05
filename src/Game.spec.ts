132c005d7c2e62b34c75921080317673020eb185
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-04 23:31:55 -0300

    test: all ones game

--- src/Game.spec.ts
+++ src/Game.spec.ts
@@ -8,6 +8,16 @@ describe('Bowling game', () => {
       g.roll(0);
     }
 
-    expect(g.score()).toBe(0);
+    expect(g.score).toBe(0);
+  });
+
+  it('should score 20 for all ones games', () => {
+    const g: Game = new Game();
+
+    for (let i = 0; i < 20; i += 1) {
+      g.roll(1);
+    }
+
+    expect(g.score).toBe(20);
   });
 });
