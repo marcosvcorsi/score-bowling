6117b43ec90097bbc4ccff17fd9c15665ce15f61
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-05 21:46:23 -0300

    feat: strike score

--- src/Game.ts
+++ src/Game.ts
@@ -16,13 +16,16 @@ export class Game {
     let frameIndex = 0;
 
     for (let frame = 0; frame < 10; frame++) {
-      if (this.isSpare(frameIndex)) {
+      if (this.rolls[frameIndex] === 10) {
+        score += 10 + this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+        frameIndex++;
+      } else if (this.isSpare(frameIndex)) {
         score += 10 + this.rolls[frameIndex + 2];
+        frameIndex += 2;
       } else {
         score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+        frameIndex += 2;
       }
-
-      frameIndex += 2;
     }
 
     return score;
